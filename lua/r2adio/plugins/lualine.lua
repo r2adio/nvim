b932b644c5929a4935128f82fa88479255134449
GauraVashisth <miya.musashi.moto1645@gmail.com>
2025-08-05 20:59:34 +0530

    feat: add image.nvim

--- lua/r2adio/plugins/lualine.lua
+++ lua/r2adio/plugins/lualine.lua
@@ -1,112 +1,112 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = {
-		{ "nvim-tree/nvim-web-devicons", lazy = true },
-	},
-	config = function()
-		local lualine = require("lualine")
-		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
-
-		local hide_in_width = function()
-			return vim.fn.winwidth(0) > 80
-		end
-
-		local diagnostics = {
-			"diagnostics",
-			sources = { "nvim_diagnostic" },
-			sections = { "error", "warn" },
-			symbols = { error = " ", warn = " " },
-			colored = false,
-			update_in_insert = false,
-			always_visible = true,
-		}
-
-		local diff = {
-			"diff",
-			colored = false,
-			symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
-			cond = hide_in_width,
-		}
-
-		local mode = {
-			"mode",
-			fmt = function(str)
-				return "-- " .. str .. " --"
-			end,
-		}
-
-		local filetype = {
-			"filetype",
-			icons_enabled = false,
-			icon = nil,
-		}
-
-		local branch = {
-			"branch",
-			icons_enabled = true,
-			icon = "",
-		}
-
-		local location = {
-			"location",
-			padding = 0,
-		}
-
-		-- Cool function for progress
-		local progress = function()
-			local current_line = vim.fn.line(".")
-			local total_lines = vim.fn.line("$")
-			local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
-			local line_ratio = current_line / total_lines
-			local index = math.ceil(line_ratio * #chars)
-			return chars[index]
-		end
-
-		local spaces = function()
-			return "Spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
-		end
-
-		lualine.setup({
-			options = {
-				icons_enabled = true,
-				theme = "rose-pine", -- Use your desired theme
-				component_separators = { left = "", right = "" },
-				section_separators = { left = "", right = "" },
-				disabled_filetypes = { "dashboard", "NvimTree", "Outline" },
-				always_divide_middle = true,
-			},
-			sections = {
-				lualine_a = { branch, diagnostics },
-				lualine_b = { mode },
-				lualine_c = { "filename" },
-				lualine_x = {
-					{
-						lazy_status.updates,
-						cond = lazy_status.has_updates,
-						color = { fg = "#ff9e64" },
-					},
-					diff,
-					spaces,
-					-- function()
-					-- 	local bac = "test"
-					-- 	return bac
-					-- end,
-					"encoding",
-					filetype,
-				},
-				lualine_y = { location },
-				lualine_z = { progress },
-			},
-			inactive_sections = {
-				lualine_a = {},
-				lualine_b = {},
-				lualine_c = { "filename" },
-				lualine_x = { "location" },
-				lualine_y = {},
-				lualine_z = {},
-			},
-			tabline = {},
-			extensions = {},
-		})
-	end,
+	-- "nvim-lualine/lualine.nvim",
+	-- dependencies = {
+	-- 	{ "nvim-tree/nvim-web-devicons", lazy = true },
+	-- },
+	-- config = function()
+	-- 	local lualine = require("lualine")
+	-- 	local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+	--
+	-- 	local hide_in_width = function()
+	-- 		return vim.fn.winwidth(0) > 80
+	-- 	end
+	--
+	-- 	local diagnostics = {
+	-- 		"diagnostics",
+	-- 		sources = { "nvim_diagnostic" },
+	-- 		sections = { "error", "warn" },
+	-- 		symbols = { error = " ", warn = " " },
+	-- 		colored = false,
+	-- 		update_in_insert = false,
+	-- 		always_visible = true,
+	-- 	}
+	--
+	-- 	local diff = {
+	-- 		"diff",
+	-- 		colored = false,
+	-- 		symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
+	-- 		cond = hide_in_width,
+	-- 	}
+	--
+	-- 	local mode = {
+	-- 		"mode",
+	-- 		fmt = function(str)
+	-- 			return "-- " .. str .. " --"
+	-- 		end,
+	-- 	}
+	--
+	-- 	local filetype = {
+	-- 		"filetype",
+	-- 		icons_enabled = false,
+	-- 		icon = nil,
+	-- 	}
+	--
+	-- 	local branch = {
+	-- 		"branch",
+	-- 		icons_enabled = true,
+	-- 		icon = "",
+	-- 	}
+	--
+	-- 	local location = {
+	-- 		"location",
+	-- 		padding = 0,
+	-- 	}
+	--
+	-- 	-- Cool function for progress
+	-- 	local progress = function()
+	-- 		local current_line = vim.fn.line(".")
+	-- 		local total_lines = vim.fn.line("$")
+	-- 		local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
+	-- 		local line_ratio = current_line / total_lines
+	-- 		local index = math.ceil(line_ratio * #chars)
+	-- 		return chars[index]
+	-- 	end
+	--
+	-- 	local spaces = function()
+	-- 		return "Spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+	-- 	end
+	--
+	-- 	lualine.setup({
+	-- 		options = {
+	-- 			icons_enabled = true,
+	-- 			theme = "rose-pine", -- Use your desired theme
+	-- 			component_separators = { left = "", right = "" },
+	-- 			section_separators = { left = "", right = "" },
+	-- 			disabled_filetypes = { "dashboard", "NvimTree", "Outline" },
+	-- 			always_divide_middle = true,
+	-- 		},
+	-- 		sections = {
+	-- 			lualine_a = { branch, diagnostics },
+	-- 			lualine_b = { mode },
+	-- 			lualine_c = { "filename" },
+	-- 			lualine_x = {
+	-- 				{
+	-- 					lazy_status.updates,
+	-- 					cond = lazy_status.has_updates,
+	-- 					color = { fg = "#ff9e64" },
+	-- 				},
+	-- 				diff,
+	-- 				spaces,
+	-- 				-- function()
+	-- 				-- 	local bac = "test"
+	-- 				-- 	return bac
+	-- 				-- end,
+	-- 				"encoding",
+	-- 				filetype,
+	-- 			},
+	-- 			lualine_y = { location },
+	-- 			lualine_z = { progress },
+	-- 		},
+	-- 		inactive_sections = {
+	-- 			lualine_a = {},
+	-- 			lualine_b = {},
+	-- 			lualine_c = { "filename" },
+	-- 			lualine_x = { "location" },
+	-- 			lualine_y = {},
+	-- 			lualine_z = {},
+	-- 		},
+	-- 		tabline = {},
+	-- 		extensions = {},
+	-- 	})
+	-- end,
 }
