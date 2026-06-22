d85ce322eb302ff8731fd0b289b96eff7fb3261e
gauravashisth <miya.musashi.moto1645@gmail.com>
2026-06-22 08:17:59 +0530

    update plugins

--- lua/plugins/vim.lua
+++ lua/plugins/vim.lua
@@ -1,4 +1,5 @@
 vim.pack.add({ "gh:tpope/vim-fugitive", "gh:airblade/vim-gitgutter" })
+vim.pack.add({ "gh:justinmk/guh.nvim" })
 
 vim.pack.add({ "gh:tpope/vim-dadbod", "gh:kristijanhusak/vim-dadbod-completion" })
 
